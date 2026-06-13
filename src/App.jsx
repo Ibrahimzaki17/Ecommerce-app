63378829b4530b8f01b1c7a0678bde3e2d4e81d8
Ibrahim zaki <ibrahimzakisiyat@gmail.com>
2026-06-13 22:02:14 +0300

    Created and Added routes and links to the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import HomePage from './pages/HomePage.jsx';
 import './App.css'
+import { Routes, Route } from 'react-router';
+import CheckOut from './pages/CheckOut.jsx';
+import Orders from './pages/Orders.jsx';
+import TrackingPage from './pages/TrackingPage.jsx';
 
 
 function App() {
 
   return(
     <> 
-      <HomePage />
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path='checkout' element={<CheckOut />} />
+      <Route path='orders' element={<Orders />} />
+      <Route path='tracking' element={<TrackingPage />} />
+    </Routes>
     </>
   );
 }
